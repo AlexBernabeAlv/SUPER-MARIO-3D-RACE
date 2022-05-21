ab43b0393a59666cf2134cceeb07d68a6104c8fb
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2022-05-21 10:08:21 +0200

    Dry Bones Update

--- Assets/Scripts/CreateDryBones.cs
+++ Assets/Scripts/CreateDryBones.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateDryBones : MonoBehaviour
-{
+public class CreateDryBones : MonoBehaviour {
 
     public GameObject dryBones;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         GameObject obj = (GameObject)Instantiate(dryBones, new Vector3(-3.0f, -0.5f, 14.0f), dryBones.transform.rotation);
         obj.transform.parent = transform;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
