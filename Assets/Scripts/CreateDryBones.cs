031a2cf5da5e7a77f0580ed52bf4e7ea0ecc3d7f
Marc-Gonzalez-Moratona <marcelinus99@gmail.com>
2022-05-25 00:01:53 +0200

    DryBones Movement

--- Assets/Scripts/CreateDryBones.cs
+++ Assets/Scripts/CreateDryBones.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CreateDryBones : MonoBehaviour {
+public class CreateDryBones : MonoBehaviour
+{
 
     public GameObject dryBones;
     public float runSpeed = 2f;
     public float turnSpeed = 90f;
     GameObject[] obj;
+    bool changeDirection = false;
+    int[] direction;
+
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         if (SceneManager.GetActiveScene().name == "LEVEL1")
         {
             obj = new GameObject[2];
-            obj[0] = (GameObject)Instantiate(dryBones, new Vector3(-3.0f, -0.5f, 14.0f), dryBones.transform.rotation);
-            obj[0].transform.Rotate(0, 180, 0);
-            obj[0].transform.parent = transform;
-            transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("IsRunning", true);
+            direction = new int[2];
+            for (int i = 0; i < 2; ++i)
+            {
+                obj[i] = (GameObject)Instantiate(dryBones, new Vector3(-3.0f + 6f * i, -0.5f, 14f), dryBones.transform.rotation);
+                obj[i].transform.Rotate(0, 180, 0);
+                obj[i].transform.parent = transform;
+                transform.GetChild(i).gameObject.GetComponent<Animator>().SetBool("IsRunning", true);
+            }
         }
-        else if (SceneManager.GetActiveScene().name == "LEVEL2") {
+        else if (SceneManager.GetActiveScene().name == "LEVEL2")
+        {
             obj = new GameObject[4];
-
+            direction = new int[4];
             for (int i = 0; i < 4; ++i)
             {
                 if (i % 2 == 0)
@@ -44,10 +54,158 @@ public class CreateDryBones : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        for(int i = 0;i<obj.Length; ++i)
+    void Update()
+    {
+        Debug.Log(direction[0]);
+        Debug.Log(obj[0].transform.rotation.eulerAngles.y);
+        for (int i = 0; i < obj.Length; ++i)
         {
-            if (obj[i].transform.position.z > 5) obj[i].transform.Translate(-transform.forward * runSpeed * Time.deltaTime, Space.World);
+            if (changeDirection)
+            {
+                if (i % 2 == 0)
+                {
+                    if (direction[i] == 0)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y <= 270)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 1)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y > 270)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 2)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y <= 90)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 3)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y <= 180)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else direction[i] = 0;
+                }
+                else
+                {
+                    if (direction[i] == 0)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y >= 90)
+                        {
+                            obj[i].transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 1)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y > 0)
+                        {
+                            obj[i].transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 2)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y >= 270)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else if (direction[i] == 3)
+                    {
+                        if (obj[i].transform.rotation.eulerAngles.y >= 180)
+                        {
+                            obj[i].transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+                        }
+                        else
+                        {
+                            changeDirection = false;
+                            direction[i]++;
+                        }
+                    }
+                    else direction[i] = 0;
+                }
+            }
+            else
+            {
+                if (direction[i] == 0)
+                {
+                    if (obj[i].transform.position.z > 9) obj[i].transform.Translate(-transform.forward * runSpeed * Time.deltaTime, Space.World);
+                    else changeDirection = true;
+                }
+                else if (direction[i] == 1)
+                {
+                    if(i%2 == 0)
+                    {
+                        if (obj[i].transform.position.x > -8) obj[i].transform.Translate(-transform.right * runSpeed * Time.deltaTime, Space.World);
+                        else changeDirection = true;
+                    } else
+                    {
+                        if (obj[i].transform.position.x < 8) obj[i].transform.Translate(transform.right * runSpeed * Time.deltaTime, Space.World);
+                        else changeDirection = true;
+                    }
+                }
+                else if (direction[i] == 2)
+                {
+                    if (obj[i].transform.position.z < 14) obj[i].transform.Translate(transform.forward * runSpeed * Time.deltaTime, Space.World);
+                    else changeDirection = true;
+                }
+                else if (direction[i] == 3)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (obj[i].transform.position.x < -3) obj[i].transform.Translate(transform.right * runSpeed * Time.deltaTime, Space.World);
+                        else changeDirection = true;
+                    }
+                    else
+                    {
+                        if (obj[i].transform.position.x > 3) obj[i].transform.Translate(-transform.right * runSpeed * Time.deltaTime, Space.World);
+                        else changeDirection = true;
+                    }
+                }
+                else direction[i] = 0;
+            }
         }
     }
 }
